f52d5424b24737c251a9481d222dcef5c64e34a1
Nikita <iniktus@mail.ru>
2022-08-02 14:00:43 +0300

    new file:   task1.zip 	renamed:    task1_BubbleSort/task1_BubbleSort/Program.cs -> task1_2_1_BubbleSort/task1_BubbleSort/Program.cs 	new file:   task1_3_1_GamesClasses/task1_3_1_GamesClasses/Class1.cs 	new file:   task1_3_1_GamesClasses/task1_3_1_GamesClasses/Program.cs

---
+++ task1_3_1_GamesClasses/task1_3_1_GamesClasses/Class1.cs
@@ -0,0 +1,178 @@
+﻿
+namespace computer_games
+{
+    class MOBA
+    {
+        private string name;
+
+        public string description;
+
+        private int year;
+
+        private int amountOfCharacters;
+
+        private int teamSize;
+
+        public MOBA(
+            string name,
+            int year,
+            int amountOfCharacters,
+            int teamSize,
+            string description)
+        {
+            this.name = name;
+            this.year = year;
+            this.amountOfCharacters = amountOfCharacters;
+            this.teamSize = teamSize;
+            this.description = description;
+        }
+        public void message() 
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Year: " + year);
+            Console.WriteLine("Amount of characters: " + amountOfCharacters);
+            Console.WriteLine("Size of team: " + teamSize);
+            Console.WriteLine("Description: " + description);
+        }
+    }
+
+    internal class Shooter
+    {
+        private string name;
+
+        public string description;
+
+        private int year;
+
+        private int amountOfGunTypes;
+
+        private int teamSize;
+
+        public Shooter(
+            string name,
+            int year,
+            int amountOfGunTypes,
+            int teamSize,
+            string description)
+        {
+            this.name = name;
+            this.year = year;
+            this.amountOfGunTypes = amountOfGunTypes;
+            this.teamSize = teamSize;
+            this.description = description;
+        }
+        public void message()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Year: " + year);
+            Console.WriteLine("Amount of gun types: " + amountOfGunTypes);
+            Console.WriteLine("Size of team: " + teamSize);
+            Console.WriteLine("Description: " + description);
+        }
+    }
+
+    internal class RPG
+    {
+        private string name;
+
+        public string description;
+
+        private int year;
+
+        private string view;
+
+        private int hoursToComplete;
+
+        public RPG(
+            string name,
+            int year,
+            string view,
+            int hoursToComplete,
+            string description)
+        {
+            this.name = name;
+            this.year = year;
+            this.view = view;
+            this.hoursToComplete = hoursToComplete;
+            this.description = description;
+        }
+        public void message()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Year: " + year);
+            Console.WriteLine("View: " + view);
+            Console.WriteLine("Hours to complete: " + hoursToComplete);
+            Console.WriteLine("Description: " + description);
+        }
+    }
+
+    internal class Puzzle
+    {
+        private string name;
+
+        public string description;
+
+        private int year;
+
+        private int complexityRating;
+
+        private int amountOfLevels;
+
+        public Puzzle(
+            string name,
+            int year,
+            int complexityRating,
+            int amountOfLevels,
+            string description)
+        {
+            this.name = name;
+            this.year = year;
+            this.complexityRating = complexityRating;
+            this.amountOfLevels = amountOfLevels;
+            this.description = description;
+        }
+        public void message()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Year: " + year);
+            Console.WriteLine("Complexity rating: " + complexityRating);
+            Console.WriteLine("Amount of levels: " + amountOfLevels);
+            Console.WriteLine("Description: " + description);
+        }
+    }
+
+    internal class Strategy
+    {
+        private string name;
+
+        public string description;
+
+        private int year;
+
+        private string historicalPeriod;
+
+        private int amountOfRivals;
+
+        public Strategy(
+            string name,
+            int year,
+            string historicalPeriod,
+            int amountOfRivals,
+            string description)
+        {
+            this.name = name;
+            this.year = year;
+            this.historicalPeriod = historicalPeriod;
+            this.amountOfRivals = amountOfRivals;
+            this.description = description;
+        }
+        public void message()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Year: " + year);
+            Console.WriteLine("Hsitorical period: " + historicalPeriod);
+            Console.WriteLine("Maximal amount of rivals: " + amountOfRivals);
+            Console.WriteLine("Description: " + description);
+        }
+    }
+}
---
+++ task1_3_1_GamesClasses/task1_3_1_GamesClasses/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        computer_games.MOBA moba = new computer_games.MOBA("League of Legends", 2009, 132, 5, "The main contender with DOTA");
+        computer_games.Shooter shooter = new computer_games.Shooter("Battlefield 4", 2013, 16, 64, "Also known as BUGField 4");
+        computer_games.Puzzle puzzle = new computer_games.Puzzle("Portal 2", 2011, 7, 64, "The best puzzle game I ever played");
+        computer_games.RPG rpg = new computer_games.RPG("Fallout 4", 2015, "1st-person, 3d-person", 400, "Buy Skyr... I mean Fallout 4");
+        computer_games.Strategy strategy = new computer_games.Strategy("Stellaris", 2016, "Far fantastic future", 20, "One of the cooles galactic empire simulators");
+
+        Console.WriteLine("\t--= MOBA =--");
+        moba.message();
+
+        Console.WriteLine("\t--= Shooter =--");
+        shooter.message();
+
+        Console.WriteLine("\t--= RPG =--");
+        rpg.message();
+
+        Console.WriteLine("\t--= Puzzle =--");
+        puzzle.message();
+
+
+        Console.WriteLine("\t--= Strategy =--");
+        strategy.message();
+    }
+}
