26fbe1ac5614dec7d66c678112cffd0b75621986
Nikita <iniktus@mail.ru>
2022-08-05 11:31:27 +0300

    Задание 1.4 - Списки и словари

---
+++ task1_4_1_Complex_Structures/task1_4_1_Complex_Structures/Enemy.cs
@@ -0,0 +1,91 @@
+﻿namespace task1_3_2_OOP_Characters
+{
+    public class Enemy
+    {
+        protected string _name;
+
+        protected Sex _sex;
+
+        protected string _race;
+
+        protected int _age;
+
+        protected int _level;
+
+        public int _maxHealthPoints;
+
+        public int currentHP;
+
+        public Enemy(string name, Sex sex, string race, int age, int level, int health)
+        {
+            this._name = name;
+            this._sex = sex;
+            this._race = race;
+            this._age = age;
+            this._level = level;
+            this._maxHealthPoints = health;
+            this.currentHP = _maxHealthPoints;
+        }
+
+        virtual public void Message()
+        {
+            Console.WriteLine("[ENEMY]");
+            Console.WriteLine("I am an enemy of " + _level + " level. My name is " + _name + " and I am " + _age + " years old.");
+            Console.WriteLine("I am " + _sex + " of " + _race + " race.");
+        }
+    }
+
+    public class Bandit : Enemy
+    {
+        private string GangName { get; set; }
+
+        private int money { get; set; }
+        public Bandit(string name, Sex sex, string race, int age, int level, int health, string gangName, int money) : base(name, sex, race, age, level, health)
+        {
+            GangName = gangName;
+            this.money = money;
+        }
+
+        public override void Message()
+        {
+            base.Message();
+            Console.WriteLine("Your money or your life!");
+        }
+    }
+
+    public class Dragon : Enemy
+    {
+        private string _TrueName;
+
+        private int money { get; set; }
+        public Dragon(string name, Sex sex, string race, int age, int level, int health, string trueName, int money) : base(name, sex, race, age, level, health)
+        {
+            _TrueName = trueName;
+            this.money = money;
+        }
+
+        public override void Message()
+        {
+            base.Message();
+            Console.WriteLine("Bow low, you insects!");
+        }
+    }
+
+    public class OrcBoyz : Enemy
+    {
+        private string FavoriteColor;
+
+        private int ToothAmount { get; set; }
+        public OrcBoyz(string name, Sex sex, string race, int age, int level, int health, string favoriteColor, int toothAmount) : base(name, sex, race, age, level, health)
+        {
+            FavoriteColor = favoriteColor;
+            this.ToothAmount = toothAmount;
+        }
+
+        public override void Message()
+        {
+            base.Message();
+            Console.WriteLine("WAAAAAAAGHH!");
+        }
+    }
+}
---
+++ task1_4_1_Complex_Structures/task1_4_1_Complex_Structures/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using task1_3_2_OOP_Characters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DictionaryTask();
+        Console.WriteLine("-----------");
+        LinkedListTask(45, 44, 50);
+    }
+
+    private static void DictionaryTask()
+    {
+        Dictionary<string, Character> characterList = new Dictionary<string, Character>();
+        characterList.Add("FuBar", new Player("FuBar", Sex.MAN, "Human", 19, 1, "Best player in da world", "Nikita", 1));
+        characterList.Add("Bob", new Peasant("Bob", Sex.MAN, "Human", 36, 4, "Regular peasant", "Kaedwen", "Richard II"));
+        characterList.Add("Alexander", new Knight("Alexander Black Jr", Sex.MAN, "Human", 28, 9, "Brave knight", "Temeria", 6));
+
+        foreach (KeyValuePair<string, Character> pair in characterList)
+        {
+            Console.WriteLine($"Key: {pair.Key}");
+            Console.WriteLine($"Value: {pair.Value}");
+        }
+    }
+
+    private static void LinkedListTask(int hp1, int hp2, int hp3)
+    {
+        LinkedList<Enemy> enemyList = new LinkedList<Enemy>();
+        enemyList.AddLast(new Bandit("Grok Blackbeard", Sex.MAN, "Kadjit", 33, 10, hp1, "Black buckaneers", 555));
+        enemyList.AddLast(new OrcBoyz("Mork", Sex.MAN, "Orc", 18, 6, hp2, "DA RED GOEZ FASTA", 16));
+        enemyList.AddLast(new Dragon("Aldringel", Sex.WOMAN, "Ancient Dragon", 786, 96, hp3, "UNKNOWN", 100000000));
+
+        bool death = false;
+
+        while (!death)
+        {
+            foreach(Enemy enemy in enemyList)
+            {
+                enemy.currentHP -= 2;
+                Console.WriteLine($"{enemy} HP: {enemy.currentHP} / {enemy._maxHealthPoints}");
+
+                if (enemy.currentHP <= 0)
+                {
+                    death = true;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Dead:");
+
+        foreach (Enemy enemy in enemyList)
+        {
+            if (enemy.currentHP <= 0)
+            {
+                Console.WriteLine($"{enemy} is dead.");
+            }
+        }
+        Console.WriteLine();
+
+        Enemy aliveEnemy = enemyList.MaxBy(keySelector: (Enemy e) => e.currentHP);
+
+        if (aliveEnemy == null)
+        {
+            Console.WriteLine("No one has survived!");
+        } else
+        {
+            Console.WriteLine("Most powerful alive:");
+            Console.WriteLine($"{aliveEnemy} : {aliveEnemy.currentHP} / {aliveEnemy._maxHealthPoints}");
+        }
+    }
+}
